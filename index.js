876f8272a5e9e5ff7bf71dd9a86243d46dc8a173
unexpectedhaki <tpritchard843@gmail.com>
2023-06-08 18:14:42 -0400

    Changed how quote is rendered upon click

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelector('#search-btn').addEventListener('click', getQuote)
+document.querySelector('#search-btn').addEventListener('click', getQuote);
 document.querySelector('#random-btn').addEventListener('click', getRandomQuote);
 
 function getQuote() {
@@ -7,27 +7,40 @@ function getQuote() {
     fetch(`http://animechan.melosh.space/random/anime?title=${input}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        console.log(data);
+
         document.querySelector('.quote').innerText = `"${data.quote}"`;
         document.querySelector('.character-name').innerText = `- ${data.character}`;
         document.querySelector('.anime-name').innerText = data.anime;
+
+        renderQuote();
     })
     .catch(err => {
         console.log(`error ${err}`);
     });
+
+    
 }
 
 function getRandomQuote() {
     fetch('http://animechan.melosh.space/random')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        console.log(data);
+        
         document.querySelector('.quote').innerText = `"${data.quote}"`;
         document.querySelector('.character-name').innerText = `- ${data.character}`;
         document.querySelector('.anime-name').innerText = data.anime;
+        
+        renderQuote();
     })
     .catch(err => {
         console.log(`error ${err}`);
     });
+
 }
 
+
+function renderQuote() {
+    document.querySelector('.quote-modal').classList.remove('hidden')
+}
