ba0aba609bb5d43b3d916f8d5766d9d38ee000e1
unexpectedhaki <tpritchard843@gmail.com>
2023-06-08 18:09:51 -0400

    Added a search feature -- can search for quotes by anime name

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-document.querySelector('#quote-btn').addEventListener('click', getQuote)
+document.querySelector('#search-btn').addEventListener('click', getQuote)
+document.querySelector('#random-btn').addEventListener('click', getRandomQuote);
 
 function getQuote() {
+    let input = document.querySelector('.input').value;
+
+    fetch(`http://animechan.melosh.space/random/anime?title=${input}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        document.querySelector('.quote').innerText = `"${data.quote}"`;
+        document.querySelector('.character-name').innerText = `- ${data.character}`;
+        document.querySelector('.anime-name').innerText = data.anime;
+    })
+    .catch(err => {
+        console.log(`error ${err}`);
+    });
+}
+
+function getRandomQuote() {
     fetch('http://animechan.melosh.space/random')
     .then(response => response.json())
     .then(data => {
@@ -10,7 +27,7 @@ function getQuote() {
         document.querySelector('.anime-name').innerText = data.anime;
     })
     .catch(err => {
-        console.log(`error ${err}`)
+        console.log(`error ${err}`);
     });
 }
 
