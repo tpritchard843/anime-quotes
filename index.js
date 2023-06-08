0399537dfbffa29d35ee38da977d3e09e73edbe8
unexpectedhaki <tpritchard843@gmail.com>
2023-06-08 15:57:07 -0400

    Added random quote generation feature

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-fetch('http://animechan.melosh.space/random')
+document.querySelector('#quote-btn').addEventListener('click', getQuote)
+
+function getQuote() {
+    fetch('http://animechan.melosh.space/random')
     .then(response => response.json())
-    .then(quote => console.log(quote))
+    .then(data => {
+        console.log(data)
+        document.querySelector('.quote').innerText = `"${data.quote}"`;
+        document.querySelector('.character-name').innerText = `- ${data.character}`;
+        document.querySelector('.anime-name').innerText = data.anime;
+    })
     .catch(err => {
         console.log(`error ${err}`)
     });
+}
+
